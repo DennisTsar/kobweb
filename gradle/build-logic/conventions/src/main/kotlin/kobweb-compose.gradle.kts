0cac98fe86f7199addf4cec4133ebaff407ce802
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-02-12 00:24:19 -0500

    beta2

--- gradle/build-logic/conventions/src/main/kotlin/kobweb-compose.gradle.kts
+++ gradle/build-logic/conventions/src/main/kotlin/kobweb-compose.gradle.kts
@@ -6,4 +6,5 @@ composeCompiler {
     // Trace markers are "pure overhead" for the JS target & needlessly increase the bundle size, but
     // must be explicitly disabled until https://youtrack.jetbrains.com/issue/KT-69900 is resolved
     includeTraceMarkers = false
+    includeSourceInformation = false
 }
