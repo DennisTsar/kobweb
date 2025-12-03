4c8062320023535606611269a88274edf4dbc05b
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-12-03 03:25:04 -0500

    AMPER

---
+++ backend/server/test/com/varabyte/kobweb/server/ApplicationTest.kt
@@ -0,0 +1,16 @@
+package com.varabyte.kobweb.server
+
+import kotlin.test.Test
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        // TODO: Make this a real test with a test conf
+//        withTestApplication({ configureRouting(ServerEnvironment.DEV, KobwebConf()) }) {
+//            handleRequest(HttpMethod.Get, "/").apply {
+//                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+//                assertThat(response.content).isEqualTo("Hello World!")
+//            }
+//        }
+    }
+}
